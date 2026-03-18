468df732387c9d3ad13a97bb8819c8e1bd48b4f3
skultetymfulop-hue <skultetymfulop@gmail.com>
2026-03-18 16:35:55 +0100

    Update adatgyujto_app.py

--- adatgyujto_app.py
+++ adatgyujto_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from streamlit_gsheets import GSheetsConnection
+from streamlit-gsheets import GSheetsConnection
 
 st.title("🧪 Google Sheets Kapcsolat Teszt")
 
